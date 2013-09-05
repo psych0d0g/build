23cbe17e3d1f86e04a5d8f410ff85f69b0b6f652
maxwen <max.weninger@gmail.com>
2013-09-06 00:13:42 +0200

    added backup support - CM (1/2)
    Change-Id: If1cbe8fa04a7cff7d455087216eca5378d5f6cd9

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -101,6 +101,16 @@ class EdifyGenerator(object):
            ");")
     self.script.append(self._WordWrap(cmd))
 
+  def RunBackup(self, command):
+    self.script.append('package_extract_file("system/bin/backuptool.sh", "/tmp/backuptool.sh");')
+    self.script.append('package_extract_file("system/bin/backuptool.functions", "/tmp/backuptool.functions");')
+    self.script.append('set_perm(0, 0, 0777, "/tmp/backuptool.sh");')
+    self.script.append('set_perm(0, 0, 0644, "/tmp/backuptool.functions");')
+    self.script.append(('run_program("/tmp/backuptool.sh", "%s");' % command))
+    if command == "restore":
+        self.script.append('delete("/system/bin/backuptool.sh");')
+        self.script.append('delete("/system/bin/backuptool.functions");')
+
   def RunPersist(self, arg):
     self.script.append('run_program("/tmp/install/bin/persist.sh", "%s");' % arg)
 
